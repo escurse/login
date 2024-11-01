17b1999988129ea233c128a014b92154efa490a5
escass <av1967@naver.com>
2024-11-01 22:12:44 +0900

    reallogin

--- src/main/java/com/escass/login/mapper/UserMapper.java
+++ src/main/java/com/escass/login/mapper/UserMapper.java
@@ -1,8 +1,9 @@
 package com.escass.login.mapper;
 
+import com.escass.login.Entities.UserEntity;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserMapper {
-
+    UserEntity insertUser(UserEntity user);
 }
