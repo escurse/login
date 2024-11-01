c503ca49084224ff485f6718d9a1b86bce0b68b7
escass <av1967@naver.com>
2024-11-01 22:18:35 +0900

    reallogin

--- src/main/java/com/escass/login/controllers/UserController.java
+++ src/main/java/com/escass/login/controllers/UserController.java
@@ -1,5 +1,17 @@
 package com.escass.login.controllers;
 
-public class UserController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
+@RequestMapping(value="/user")
+public class UserController {
+    @RequestMapping(value="/login", method = RequestMethod.GET)
+    public ModelAndView getLogin() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("user/login");
+        return modelAndView;
+    }
 }
