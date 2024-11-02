f9ed5adf4e4df9afa3ab4736bb8682baed01264d
escass <av1967@naver.com>
2024-11-02 11:06:24 +0900

    reallogin

--- src/main/java/com/escass/login/controllers/UserController.java
+++ src/main/java/com/escass/login/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.escass.login.controllers;
 import com.escass.login.Entities.UserEntity;
 import com.escass.login.mapper.UserMapper;
 import com.escass.login.services.UserService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -11,34 +12,42 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(value="/user")
+@RequestMapping(value = "/user")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
     private final UserMapper userMapper;
 
-    @RequestMapping(value="/login", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+    @RequestMapping(value = "/login", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
     public ModelAndView getLogin() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("user/login");
         return modelAndView;
     }
 
-    @RequestMapping(value="/login", method = RequestMethod.POST)
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
     public ModelAndView postLogin(UserMapper userMapper) {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("user/login");
         return modelAndView;
     }
 
-    @RequestMapping(value="/register", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public ModelAndView getLogout(HttpSession session) {
+        session.invalidate();
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("user/login");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
     public ModelAndView getRegister() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("user/register");
         return modelAndView;
     }
 
-    @RequestMapping(value="/register", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/register", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public ModelAndView postRegister() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("user", new UserEntity());
